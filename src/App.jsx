f927192445cbd37101066f492484f7676bf72f87
Kirill Sheva <kirillsheva80@gmail.com>
2024-09-21 22:11:07 +0300

    Pizza

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,34 @@
 import React from "react";
+import axios from "axios"
 import {BrowserRouter, Routes, Route} from "react-router-dom"
 import Home from "./components/Home";
 import Cart from "./components/Cart";
 
 function App() {
-  const [allProducts, setAllProducts]  = React.useState(
-    [
-      {"name": "Чизбургер-пицца meat", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "meat"},
-      {"name": "Чизбургер-пицца meat", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "meat"},
-      {"name": "Чизбургер-пицца meat", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "meat"},
-      {"name": "Чизбургер-пицца veget", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "veget"},
-      {"name": "Чизбургер-пицца veget", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "veget"},
-      {"name": "Чизбургер-пицца veget", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "veget"},
-      {"name": "Чизбургер-пицца grill", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "grill"},
-      {"name": "Чизбургер-пицца grill", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "grill"},
-      {"name": "Чизбургер-пицца grill", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "grill"},
-      {"name": "Чизбургер-пицца sharp", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "sharp"},
-      {"name": "Чизбургер-пицца sharp", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "sharp"},
-      {"name": "Чизбургер-пицца sharp", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "sharp"},
-      {"name": "Чизбургер-пицца closed", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "closed"},
-      {"name": "Чизбургер-пицца closed", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "closed"},
-      {"name": "Чизбургер-пицца closed", "price": "от 395 ₽", "dough": "delicate", "size": "26 см.", "imgUrl": "https://dodopizza-a.akamaihd.net/static/Img/Products/Pizza/ru-RU/b750f576-4a83-48e6-a283-5a8efb68c35d.jpg", "category": "closed"},
-    ]
-  )
+  const [allProducts, setAllProducts]  = React.useState([])
+  const [cartPoducts, setCartPoducts] = React.useState([])
 
+  const onAddClick = (prod) => {
+    for (let i = 0; i < cartPoducts.length; i++) {
+      if (prod.key === cartPoducts[i].key) {console.log(prod);return} 
+    }
+    axios.post("https://66e34a13494df9a478e4db0d.mockapi.io/cart", prod)
+    axios.get("https://66e34a13494df9a478e4db0d.mockapi.io/cart").then(res => {
+      setCartPoducts(res.data)  
+    })
+    console.log(cartPoducts);
+    
+  }
 
+  React.useEffect(() => {
+    axios.get("https://66e34a13494df9a478e4db0d.mockapi.io/products").then(res => setAllProducts(res.data))
+  }, [])
 
   return (
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route element={<Home allProducts={allProducts} />} path="/" />
+          <Route element={<Home allProducts={allProducts} onAddClick={(prod) => onAddClick(prod)}/>} path="/" />
           <Route element={<Cart />} path="/cart"/>
         </Routes>
       </BrowserRouter>
